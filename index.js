6ce4abcfc4e840b68891bc2d5ff7fa3e4e6c11d2
entcheva <christina.entcheva@gmail.com>
2017-01-15 21:39:04 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 const app = "I don't do much."
+
+var destructivelyAppendKitten = name => {
+  kittens.push(name);
+};
+
+var destructivelyPrependKitten = name => {
+  kittens.unshift(name);
+};
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop(kittens.length-1);
+};
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+};
+
+var appendKitten = name => {
+  return [...kittens, name];
+};
+
+var prependKitten = name => {
+  return [name, ...kittens];
+};
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length-1);
+};
+
+function removeFirstKitten() {
+  return kittens.slice(1);
+};
